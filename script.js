57792e277717dacd119e4d1f82ae80122247a700
shiwani08 <shiwanisoni29082002@gmail.com>
2025-04-14 12:12:22 +0530

    Dynamic notes creation

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('test')
+const addBtn = document.getElementById('addNote')
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
+addBtn.addEventListener('click', () => addNewNote())
+
+function addNewNote (text = '') {
+    const note = document.createElement('div')
+    note.classList.add('note')
+
+    note.innerHTML = `
+    <div class = "tools">
+        <button class="edit">E</button>
+        <button class="delete">D</button>
+    </div>
+
+    <div class = "main ${text? "" : "hidden"}"></div>
+    <textarea class="textarea ${text ? 'hidden' : ''}"></textarea>
+    `
+    const editBtn = note.querySelector('.edit')
+    const deleteBtn = note.querySelector('.delete')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('.textarea')
+
+    textArea.value = text
+    main.innerHTML = marked.parse(text)
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove()
+
+        updateLS()
+    })
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    })
+
+    main.addEventListener('click', () => {
+        main.classList.add('hidden')
+        textArea.classList.remove('hidden')
+    })
+
+    textArea.addEventListener('input', (e) => {
+        const {value} = e.target
+        main.innerHTML = marked.parse(value)
+        updateLS()
+    })
+
+    const container = document.querySelector('.notesContainer')
+    container.appendChild(note)
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
+    const notes= []
+    notesText.forEach(note => notes.push(note.value))
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
